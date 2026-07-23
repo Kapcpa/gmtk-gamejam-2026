197fe4b2119d41d88647810726eaaa98a4168d1d
Kapcpa <kacpa2006@gmail.com>
2026-07-23 12:59:56 +0200

    scene transitions

---
+++ scripts/transition_manager.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+@onready var color_rect: ColorRect = $ColorRect
+
+func transition_to(scene_path: String) -> void:
+	var tween = create_tween()
+	tween.tween_property(color_rect, "color:a", 1.0, 0.5)
+	await tween.finished
+	
+	get_tree().change_scene_to_file(scene_path)
+	
+	var tween_back = create_tween()
+	tween_back.tween_property(color_rect, "color:a", 0.0, 0.5)
