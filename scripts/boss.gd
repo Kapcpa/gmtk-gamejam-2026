14a36b325cf3d66fd2b30435c2785a917ead8633
Koneser <koneser4822@pm.me>
2026-07-26 14:51:01 +0200

    boss finished

--- scripts/boss.gd
+++ scripts/boss.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+class_name EnemyCharacter
+
 @onready var player: PlayerCharacter = %player
 @onready var tilemap: TileMapLayer = %tilemap
 
@@ -20,9 +22,9 @@ extends CharacterBody2D
 @export var SPEED = 70.0
 @export var ATTACK_SPEED = 200.0
 @export var RANGED_ATTACK_SPEED = -200.0
-@export var CHARGING_ATTACK_SPEED = 1000.0
+@export var CHARGING_ATTACK_SPEED = 800.0
 @export var ATTACK_FRICTION = 1000
-@export var CHARGING_TIME = 0.7
+@export var CHARGING_TIME = 0.8
 @export var MELEE_ATTACK_COOLDOWN: float = 1.0
 @export var RANGED_ATTACK_COOLDOWN: float = 3.0
 @export var LASER_ATTACK_COOLDOWN: float = 6.0
@@ -31,11 +33,15 @@ extends CharacterBody2D
 @export var vision: int = 20
 @export var bullets_in_a_row: int = 3
 @export var CHARGES_COUNT: int = 3
+@export var INVINCIBILITY_TIME: float = 0.5
 
 enum State {
 	IDLE,
 	RUNNING,
-	ATTACKING,
+	SHOOTING_WITH_GUN,
+	CHARGED,
+	ATTACKING_WITH_MELEE,
+	SHOOTING_LASER,
 	CHARGING,
 	HIT,
 	DEAD
@@ -55,6 +61,8 @@ var charging_direction: Vector2 = Vector2.ZERO
 var already_shaken: bool = false
 var attack_type: String = ""
 var charges_left: int = 0
+var last_laser_target_position: Vector2 = Vector2.ZERO
+var invincibility_timer: float = 0.0
 
 var pathfinding_grid: AStarGrid2D
 var path: PackedVector2Array
@@ -86,17 +94,24 @@ func _physics_process(delta: float) -> void:
 	if not player:
 		return
 	
+	if invincibility_timer > 0.0:
+		invincibility_timer -= delta
+	
 	match current_state:
 		State.IDLE:
 			_state_idle(delta)
 		State.RUNNING:
 			_state_running(delta)
-		State.ATTACKING:
-			_state_attacking(delta)
+		State.SHOOTING_WITH_GUN:
+			_state_shooting_gun(delta)
+		State.CHARGED:
+			_state_charged(delta)
+		State.ATTACKING_WITH_MELEE:
+			_state_attacking_melee(delta)
+		State.SHOOTING_LASER:
+			_state_shooting_laser(delta)
 		State.CHARGING:
 			_state_charging(delta)
-		State.HIT:
-			_state_hit(delta)
 		State.DEAD:
 			_state_dead(delta)
 		_:
@@ -176,70 +191,103 @@ func _state_running(_delta: float) -> void:
 	
 	velocity = direction * SPEED
 
-func _start_attacking() -> void:
-	if bullets_in_a_row != 0:
-		attack_timer = bullets_in_a_row/10.0
-	else:
-		attack_timer = 0.25
-	
+func _start_attacking() -> void:	
 	if attack_type == "ranged":
+		if bullets_in_a_row > 0:
+			attack_timer = bullets_in_a_row/10.0
+		
 		var direction = global_position.direction_to(player.global_position)
 		velocity = direction.normalized() * RANGED_ATTACK_SPEED
-		_change_state(State.ATTACKING)
+		_change_state(State.SHOOTING_WITH_GUN)
 		return
 	if attack_type == "charging":
+		attack_timer = 0.25
 		charging_timer = CHARGING_TIME
 		charging_direction = Vector2.ZERO
 		charges_left = CHARGES_COUNT
 		_change_state(State.CHARGING)
 		return
 	if attack_type == "melee":
-		charging_timer = CHARGING_TIME
+		attack_timer = 0.25
+		charging_timer = 0.5
 		_change_state(State.CHARGING)
 		return
 	if attack_type == "laser":
-		charging_timer = CHARGING_TIME
+		attack_timer = 2.0
+		charging_timer = 2.0
 		_change_state(State.CHARGING)
 		return
 
-func _state_attacking(_delta: float) -> void:
+func _state_shooting_gun(_delta: float) -> void:
 	_flip_sprite()
-	
-	if attack_type == "ranged":
-		velocity = velocity.move_toward(Vector2.ZERO, ATTACK_FRICTION * _delta)
-		attack.attack_ranged(player, bullets_in_a_row)
-		ranged_attack_cooldown_timer = RANGED_ATTACK_COOLDOWN
-		print("Boss used a gun")
-	if attack_type == "charging":
-		attack.attack_melee(player)
-		charging_attack_cooldown_timer = CHARGING_ATTACK_COOLDOWN
-		print("Boss charged")
-		charges_left -= 1
-	if attack_type == "melee":
-		attack.attack_melee(player)
-		melee_attack_cooldown_timer = MELEE_ATTACK_COOLDOWN
-		print("Boss punched")
-	if attack_type == "laser":
-		attack.attack_laser(player)
-		laser_attack_cooldown_timer = LASER_ATTACK_COOLDOWN
-		print("Boss used a laser")
+	velocity = velocity.move_toward(Vector2.ZERO, ATTACK_FRICTION * _delta)
+	attack.attack_ranged(player, bullets_in_a_row)
 	
 	attack_timer -= _delta
-	melee_attack_cooldown_timer -= _delta
-	ranged_attack_cooldown_timer -= _delta
-	laser_attack_cooldown_timer -= _delta
-	charging_attack_cooldown_timer -= _delta
+	
 	if attack_timer <= 0.0:
 		attack.reset()
-		if attack_type == "charging":
-			if not charges_left:
-				attack_type = ""
-				_change_state(State.IDLE)
-				return
-			elif charging_attack_cooldown_timer <= 0.0:
-				charging_timer = CHARGING_TIME
-				_change_state(State.CHARGING)
-				return
+		attack_type = ""
+		ranged_attack_cooldown_timer = RANGED_ATTACK_COOLDOWN
+		_change_state(State.IDLE)
+
+func _state_charged(_delta: float) -> void:
+	velocity = velocity.move_toward(Vector2.ZERO, ATTACK_FRICTION * _delta)
+	_flip_sprite()
+	attack.attack_melee(player)
+	
+	attack_timer -= _delta
+	
+	if attack_timer <= 0.0:
+		attack.reset()
+		if not charges_left:
+			attack_type = ""
+			charging_attack_cooldown_timer = CHARGING_ATTACK_COOLDOWN
+			_change_state(State.IDLE)
+			return
+		else:
+			charging_timer = CHARGING_TIME
+			attack_timer = 0.25
+			_change_state(State.CHARGING)
+
+func _state_attacking_melee(_delta: float) -> void:
+	velocity = velocity.move_toward(Vector2.ZERO, ATTACK_FRICTION * _delta)
+	_flip_sprite()
+	attack.attack_melee(player)
+	
+	attack_timer -= _delta
+	
+	if attack_timer <= 0.0:
+		attack.reset()
+		attack_type = ""
+		melee_attack_cooldown_timer = MELEE_ATTACK_COOLDOWN
+		_change_state(State.IDLE)
+
+func _state_shooting_laser(_delta: float) -> void:
+	velocity = velocity.move_toward(Vector2.ZERO, ATTACK_FRICTION * _delta)
+	_flip_sprite()
+	thick_laser.show()
+	thick_laser.points = [
+		Vector2.ZERO,
+		last_laser_target_position
+	]
+	laser_raycast.target_position = last_laser_target_position
+	if laser_raycast.is_colliding():
+		player.take_damage(300 * global_position.direction_to(player.global_position))
+	
+	attack_timer -= _delta
+	laser_attack_cooldown_timer -= _delta
+	
+	if attack_timer <= 0.0:
+		thick_laser.hide()
+		attack.reset()
+		attack_type = ""
+		laser_attack_cooldown_timer = LASER_ATTACK_COOLDOWN
+		_change_state(State.IDLE)
+
+func _state_attacking(_delta: float) -> void:
+	if attack_timer <= 0.0:
+		attack.reset()
 		attack_type = ""
 		_change_state(State.IDLE)
 
@@ -248,32 +296,37 @@ func _state_charging(_delta: float) -> void:
 	charging_timer -= _delta
 	velocity = velocity.move_toward(Vector2.ZERO, ATTACK_FRICTION * _delta)
 	
-	if attack_type == "charging" and charges_left:
+	if attack_type == "charging":
 		if charging_direction == Vector2.ZERO:
 			charging_direction = global_position.direction_to(player.global_position)
 		if charging_timer <= 0.0:
 			var dash_right: GPUParticles2D = $dash_right
 			dash_right.restart()
 			velocity = charging_direction.normalized() * CHARGING_ATTACK_SPEED
-			_change_state(State.ATTACKING)
+			charges_left -= 1
+			_change_state(State.CHARGED)
 			charging_direction = Vector2.ZERO
 			
 	if attack_type == "melee" and charging_timer <= 0.0:
 		var direction = global_position.direction_to(player.global_position)
 		velocity = direction.normalized() * ATTACK_SPEED
-		_change_state(State.ATTACKING)
+		_change_state(State.ATTACKING_WITH_MELEE)
 	
-	if attack_type == "laser" and charging_timer <= 0.0:
-		var direction = global_position.direction_to(player.global_position)
-		velocity = direction.normalized() * RANGED_ATTACK_SPEED
-		_change_state(State.ATTACKING)
-
-func _state_hit(_delta: float) -> void:
-	velocity = knockback
-	knockback = velocity.move_toward(Vector2.ZERO, 750 * _delta) 
-	
-	if knockback == Vector2.ZERO:
-		_change_state(State.IDLE)
+	if attack_type == "laser":
+		thin_laser.show()
+		if charging_timer >= 0.35:
+			thin_laser.points = [
+			Vector2.ZERO,
+			to_local(player.position)
+			]
+		if charging_timer <= 0.35 and charging_timer >= 0.25:
+			var direction = global_position.direction_to(player.global_position)
+			last_laser_target_position = direction.normalized() * 1000000
+		if charging_timer <= 0.0:
+			var direction = global_position.direction_to(player.global_position)
+			thin_laser.hide()
+			velocity = direction.normalized() * RANGED_ATTACK_SPEED
+			_change_state(State.SHOOTING_LASER)
 
 func _state_dead(_delta: float) -> void:	
 	if death_sound.playing:
@@ -312,8 +365,10 @@ func _change_state(new_state: State) -> void:
 	current_state = new_state
 
 func take_damage(damage: float, knockback_force: Vector2) -> void:
-	if current_state in [State.HIT, State.DEAD]:
+	if invincibility_timer > 0.0:
 		return
+	print("got hit")
+	invincibility_timer = INVINCIBILITY_TIME
 	
 	if damage > 0:
 		damage_sound.play()
@@ -321,7 +376,6 @@ func take_damage(damage: float, knockback_force: Vector2) -> void:
 	health -= damage
 	
 	knockback = knockback_force
-	_change_state(State.HIT)
 	
 	if health <= 0:
 		death_sound.play()
--- scripts/boss_attack.gd
+++ scripts/boss_attack.gd
@@ -5,13 +5,15 @@ extends Area2D
 @onready var gun_burst: AudioStreamPlayer2D = $GunBurst
 @onready var gun_single: AudioStreamPlayer2D = $GunSingle
 @onready var laser_raycast: RayCast2D = $LaserRaycast
-@onready var thick_laser: Line2D = $LaserRaycast/thick_laser
-@onready var thin_laser: Line2D = $LaserRaycast/thin_laser
 
 var bullets_shot: int = 0
 var bullet_cooldown = 0.0
 
 const ATTACK_FORCE = 300
+
+func _process(delta: float) -> void:
+	if bullet_cooldown > 0.0:
+		bullet_cooldown -= delta
 
 func attack_melee(player: PlayerCharacter):
 	if player in get_overlapping_bodies() and player.current_state not in [player.State.DASHING, player.State.GRAPPLING]:
@@ -22,13 +24,15 @@ func attack_ranged(player: PlayerCharacter, bullets_in_a_row):
 	if bullets_shot >= bullets_in_a_row:
 		return
 	if bullet_cooldown <= 0.0:
+		bullet_cooldown = 0.1
+		print("bullet should shot")
 		var direction = (player.global_position - global_position).normalized()
 		var angle = Vector2(0, -1).angle_to(direction)
 		var bullet = bullet_scene.instantiate()
 		bullet.rotation = angle
 		bullet.global_position = global_position
 		bullet.player = player
-		var game_manager = get_tree().root.get_child(0)
+		var game_manager = GameManager
 		
 		game_manager.add_child(bullet)
 		
@@ -38,10 +42,6 @@ func attack_ranged(player: PlayerCharacter, bullets_in_a_row):
 			gun_burst.play()
 		
 		bullets_shot += 1
-		bullet_cooldown = 0.1
-
-func attack_laser(player: PlayerCharacter):
-	pass
 
 func reset():
 	bullets_shot = 0
